5a137ba1912d2031b9eba9a7421b2749c356ab4f
kata-agata <agata.k.jazdzewska@gmail.com>
2020-09-17 11:29:35 +0200

    carts repository added

---
+++ repositories/carts.js
@@ -0,0 +1,7 @@
+const Repository = require('./repository');
+
+class CartsRepository extends Repository{
+
+}
+
+module.export = new CartsRepository('carts.json');
---
+++ routes/carts.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+//post when clicking 'add to cart' button
+router.post('')
+
+
+
+//get to show all items in our cart
+
+
+//delete button to delete item inside cart
+
+
+module.exports = router;
